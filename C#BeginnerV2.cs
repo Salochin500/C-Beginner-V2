e962c16e717d814490d76da94d569bb39781c06d
Salochin500 <110814545+Salochin500@users.noreply.github.com>
2022-09-22 11:01:23 +1000

    Create C#BeginnerV2.cs

---
+++ C#BeginnerV2.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeThing
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+
+
+
+            // V2
+
+
+
+
+
+            // How to make Words In The exe write Console.WriteLine("Your word");
+            Console.WriteLine("exe");
+
+
+
+
+
+            Console.WriteLine("V2");
+
+
+
+
+            // Have Fun Messing With The Code
+
+
+
+
+            //e
+
+            int i = 5;
+            if (i < 10 && i == 5) {
+            }
+
+
+
+        // switch and case
+            switch (i)
+            {
+                default:
+
+                case 1:
+                    Console.WriteLine("1st case!");
+                    break;
+                case 5:
+                    Console.WriteLine("5th Case!");
+                    break;
+            }
+
+
+            // Also Forgot
+
+            int[] intArray = new int[] { 1, 2, 3, 4, 5 };
+
+            List<int> intList = new List<int>() { 1, 2, 3 };
+
+          
+            
+
+
+
+
+
+            // I forgot
+
+            if (i < 10) {
+                Console.WriteLine("True!");
+
+
+            }
+            
+
+
+
+            // hello world
+            Console.WriteLine("Hello World! " + i);
+
+
+            Console.WriteLine("Hi Robot");
+
+
+
+
+            // Strat a funtion Bool
+
+            TestFunciton();
+            bool b = SecondTestFunciton(50);
+
+
+
+
+            Console.ReadKey();
+        }
+        // funtions
+       static void TestFunciton() { 
+        }
+        //secondFuntion
+     static bool SecondTestFunciton(int i) {
+            return i < 100;
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// More Coming In the V3
